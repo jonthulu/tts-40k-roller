6899983eb8a0fb5ec5749fcfbf0e14ad9d57f525
Jon White <jon.white@clevyr.com>
2020-07-02 11:52:23 -0500

    Fixed several bugs and included a test script The Test script will include the dieRoller and add in missing global functions.

--- dieRoller.lua
+++ dieRoller.lua
@@ -2,7 +2,7 @@
 lastAttack = nil
 dieLimit = 300
 
-local function resetLastAttack()
+function resetLastAttack()
     lastAttack = {
       state = nil,
       hitSkill = nil,
@@ -24,21 +24,19 @@ end
 
 resetLastAttack()
 
-onChat('hit 20 3')
-
 function onChat(message, player)
     local isHitRoll = string.find(message, 'hit ')
     if isHitRoll == 1 then
         onHitRoll(message, player)
         return
     end
-    
+
     local isWoundRoll = string.find(message, 'wound ')
     if isWoundRoll == 1 then
         onWoundRoll(message, player)
         return
     end
-    
+
     local isSaveRoll = string.find(message, 'save ')
     if isSaveRoll == 1 then
         onSaveRoll(message, player)
@@ -50,7 +48,7 @@ function onChat(message, player)
         onIgnoreWoundRoll(message, player)
         return
     end
-    
+
     local isReRoll = string.find(message, 'reroll ')
     if isReRoll == 1 then
         onReRoll(message, player)
@@ -58,7 +56,7 @@ function onChat(message, player)
     end
 end
 
-local function onHitRoll(message, player)
+function onHitRoll(message, player)
     local skill, dieCount = string.match(message, 'hit ([2-6])+? (%d+d?[36]?)')
     if isEmpty(skill) or isEmpty(dieCount) then
         printError('Command: hit *Skill *Attacks')
@@ -68,28 +66,28 @@ local function onHitRoll(message, player)
     end
 
     -- TODO: Add support for dice dieCount (3d6 or 5d3) (already in the string.match)
-    
+
     if isOverDieLimit(dieCount) then
         return
     end
-    
+
     resetLastAttack()
-    
+
     local values, results = throwD6(dieCount, skill)
-    
+
     -- Output the hit roll numbers in case a rule will activate on a number.
     printDieValues('Hit rolls: ', values, player)
-    
+
     local hitOutput = string.format("%s Hits  |  %s Misses", results.hits, results.misses)
     printToAll(hitOutput, playerToPrintColor(player))
-    
+
     lastAttack.state = 'hit'
     lastAttack.hitSkill = skill
     lastAttack.hitValues = values
     lastAttack.hitResults = results
 end
 
-local function onWoundRoll(message, player)
+function onWoundRoll(message, player)
     local skill, dieCount = string.match(message, 'wound ([2-6])+? ?(%d*)')
     if isEmpty(skill) then
         printError('Command: wound *Skill [*Hits]')
@@ -97,27 +95,27 @@ local function onWoundRoll(message, player)
         printError('  *Hits - Optional - The number of hits (integer). This is not needed following a hit command.')
         return
     end
-    
+
     if isEmpty(dieCount) then
-        if lastAttack.state != 'hit' then
+        if lastAttack.state ~= 'hit' then
             printError('You must provide the number of dice unless rolling Wound after a Hit roll.')
             return
         end
-        
+
         dieCount = lastAttack.hitResults.hits
     else
         resetLastAttack()
     end
-    
+
     if isOverDieLimit(dieCount) then
         return
     end
-    
+
     local values, results = throwD6(dieCount, skill)
-    
+
     -- Output the wound roll numbers in case a rule will activate on a number.
     printDieValues('Wound rolls: ', values, player)
-    
+
     local woundOutput = string.format("%s Wounds  |  %s Misses", results.hits, results.misses)
     printToAll(woundOutput, playerToPrintColor(player))
 
@@ -127,7 +125,7 @@ local function onWoundRoll(message, player)
     lastAttack.woundResults = results
 end
 
-local function onSaveRoll(message, player)
+function onSaveRoll(message, player)
     local skill, dieCount = string.match(message, 'save ([2-6])+? ?(%d*)')
     if isEmpty(skill) then
         printError('Command: save *Skill [*Wounds]')
@@ -135,27 +133,27 @@ local function onSaveRoll(message, player)
         printError('  *Wounds - Optional - The number of wounds (integer). This is not needed following a wound command.')
         return
     end
-    
+
     if isEmpty(dieCount) then
-        if lastAttack.state != 'wound' then
+        if lastAttack.state ~= 'wound' then
             printError('You must provide the number of dice unless rolling Save after a Wound roll.')
             return
         end
-        
+
         dieCount = lastAttack.woundResults.hits
     else
         resetLastAttack()
     end
-    
+
     if isOverDieLimit(dieCount) then
         return
     end
-    
+
     local values, results = throwD6(dieCount, skill)
-    
+
     -- Output the save roll numbers in case a rule will activate on a number.
     printDieValues('Save rolls: ', values, player)
-    
+
     local saveOutput = string.format("%s Saved  |  %s Unsaved", results.hits, results.misses)
     printToAll(saveOutput, playerToPrintColor(player))
 
@@ -165,10 +163,10 @@ local function onSaveRoll(message, player)
     lastAttack.saveResults = results
 end
 
-local function onIgnoreWoundRoll(message, player)
+function onIgnoreWoundRoll(message, player)
     local skill, dieCount = string.match(message, 'ignore ([2-6])+? x(d?%d)')
     local skillAlt, dieCountAlt = string.match(message, 'ignore ([2-6])+? ?(%d*)')
-    
+
     if isEmpty(skill) and isEmpty(skillAlt) then
         printError('Command: ignore *Skill [*Multiplier] or [*Damage]')
         printError('  *Skill - 2, 3, 4, 5, or 6 - The value that designates what is needed to ignore the wound.')
@@ -176,15 +174,15 @@ local function onIgnoreWoundRoll(message, player)
         printError('  *Damage - Optional - The amount of damage (integer). This is not needed following a wound or save command.')
         return
     end
-    
+
     -- TODO: Add support for dice Multiplier (xd3 or xd6) (already in the string.match)
-    
+
     if not isEmpty(skill) then
-        if lastAttack.state != 'wound' and lastAttack.state != 'save' then
+        if lastAttack.state ~= 'wound' and lastAttack.state ~= 'save' then
             printError('You can not provide a multiplier unless rolling Ignore Wounds after a Wound roll or Save roll.')
             return
         end
-        
+
         if lastAttack.state == 'wound' then
             dieCount = lastAttack.woundResults.hits * dieCount
         else
@@ -192,11 +190,11 @@ local function onIgnoreWoundRoll(message, player)
         end
     else
         if isEmpty(dieCount) then
-            if lastAttack.state != 'wound' and lastAttack.state != 'save' then
+            if lastAttack.state ~= 'wound' and lastAttack.state ~= 'save' then
                 printError('You must provide the number of dice unless rolling Ignore Wounds after a Wound roll or Save roll.')
                 return
             end
-            
+
             if lastAttack.state == 'wound' then
                 dieCount = lastAttack.woundResults.hits
             else
@@ -204,22 +202,22 @@ local function onIgnoreWoundRoll(message, player)
             end
         else
             dieCount = dieCountAlt
-            
+
             resetLastAttack()
         end
-        
+
         skill = skillAlt
     end
-    
+
     if isOverDieLimit(dieCount) then
         return
     end
-    
+
     local values, results = throwD6(dieCount, skill)
-    
+
     -- Output the ignore wound roll numbers in case a rule will activate on a number.
     printDieValues('Ignore Wound rolls: ', values, player)
-    
+
     local saveOutput = string.format("%s Ignored  |  %s Damage Taken", results.hits, results.misses)
     printToAll(saveOutput, playerToPrintColor(player))
 
@@ -229,14 +227,14 @@ local function onIgnoreWoundRoll(message, player)
     lastAttack.ignoreResults = results
 end
 
-local function onReroll(message, player)
+function onReroll(message, player)
     local reRollValue = string.match(message, 'reroll ([1%*])')
     if isEmpty(reRollValue) then
         printError('Command: reroll *Values')
         printError('  *Values - 1 or * - 1 re-rolls all 1s; * re-rolls all fails.')
         return
     end
-    
+
     local previousValues = nil
     local previousSkill = nil
     if lastAttack.state == 'hit' then
@@ -249,7 +247,7 @@ local function onReroll(message, player)
         printError('You can only re-roll following a Hit or Wound roll.')
         return
     end
-    
+
     local dieCount = 0
     if reRollValue == '1' then
         dieCount = previousValues['1']
@@ -260,13 +258,13 @@ local function onReroll(message, player)
             previousValues[i] = 0
         end
     end
-    
+
     local combinedValues, combinedResults = reRollD6(dieCount, previousSkill, previousValues)
-    
+
     if lastAttack.state == 'hit' then
         -- Output the re-rolled hit roll numbers in case a rule will activate on a number.
         printDieValues('Rerolled Hits: ', combinedValues, player)
-        
+
         local hitOutput = string.format("%s Hits  |  %s Misses", combinedResults.hits, combinedResults.misses)
         printToAll(hitOutput, playerToPrintColor(player))
 
@@ -276,7 +274,7 @@ local function onReroll(message, player)
     elseif lastAttack.state == 'wound' then
         -- Output the re-rolled wound roll numbers in case a rule will activate on a number.
         printDieValues('Rerolled Wounds: ', combinedValues, player)
-        
+
         local woundOutput = string.format("%s Wounds  |  %s Misses", combinedResults.hits, combinedResults.misses)
         printToAll(woundOutput, playerToPrintColor(player))
 
@@ -286,18 +284,19 @@ local function onReroll(message, player)
     end
 end
 
-local function throwD6(dieCount, skill)
+function throwD6(dieCount, skill)
     local thrownDice = {0, 0, 0, 0, 0, 0}
     local results = {hits = 0, misses = 0}
-    
+    local safeSkill = tonumber(skill)
+
     for i=1,dieCount do
         local d = math.floor(math.random() * 6 + 1)
-        
+
         thrownDice[d] = thrownDice[d] + 1
-        
+
         if not isEmpty(skill) then
-            if d >= skill then
-                results.hit = results.hit + 1
+            if d >= safeSkill then
+                results.hits = results.hits + 1
             else
                 results.misses = results.misses + 1
             end
@@ -307,33 +306,34 @@ local function throwD6(dieCount, skill)
     return thrownDice, results
 end
 
-local function reRollD6(dieCount, skill, previousValues)
+function reRollD6(dieCount, skill, previousValues)
     local thrownDice = {0, 0, 0, 0, 0, 0}
     local results = {hits = 0, misses = 0}
-    
+    local safeSkill = tonumber(skill)
+
     for i=1, #thrownDice do
-        if i >= skill then
+        if i >= safeSkill then
             thrownDice[i] = previousValues[i]
         end
     end
 
     for i=1,dieCount do
         local d = math.floor(math.random() * 6 + 1)
-        
+
         thrownDice[d] = thrownDice[d] + 1
-        
+
         if not isEmpty(skill) then
-            if d >= skill then
-                results.hit = results.hit + 1
+            if d >= safeSkill then
+                results.hits = results.hits + 1
             else
                 results.misses = results.misses + 1
             end
         end
     end
-    
+
     for i=1, #thrownDice do
-        if thrownDice[i] >= skill then
-            results.hit = results.hit + 1
+        if thrownDice[i] >= safeSkill then
+            results.hits = results.hits + 1
         else
             results.misses = results.misses + 1
         end
@@ -342,40 +342,40 @@ local function reRollD6(dieCount, skill, previousValues)
     return thrownDice, results
 end
 
-local function isOverDieLimit(dieCount)
-    if dieCount > dieLimit then
+function isOverDieLimit(dieCount)
+    if tonumber(dieCount) > dieLimit then
         -- Cannot roll more than dieLimit die
         printToAll(string.format("Cannot roll more than %s die.", dieLimit))
         return true
     end
-    
+
     return false
 end
 
-local function playerToPrintColor(player)
+function playerToPrintColor(player)
     return player
 end
 
-local function printError(message)
+function printError(message)
     print(message)
 end
 
-local function printDieValues(prefix, dieValues, player)
+function printDieValues(prefix, dieValues, player)
     local output = ''
-    
+
     for i=1, #dieValues do
-        if dieValues[i] != 0 then
-            if i != 1 then
+        if dieValues[i] ~= 0 then
+            if i ~= 1 then
                 output = output .. "  |  "
             end
 
             output = output .. i .. ": " .. dieValues[i]
         end
     end
-    
+
     printToAll(prefix .. output, playerToPrintColor(player))
 end
 
-local function isEmpty(item)
+function isEmpty(item)
     return item == nil or item == ''
 end
---
+++ test.lua
@@ -0,0 +1,8 @@
+require "dieRoller"
+
+function printToAll(message)
+  print(message)
+end
+
+onChat('hit 3 30', 'Green')
+
